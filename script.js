de1205cc1a6a46abbc115d71bf19b76a1ff827ff
vezuras <74520628+vezuras@users.noreply.github.com>
2024-06-26 14:11:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Initialize the map\nvar map = L.map("map").setView([51.505, -0.09], 13);\n\n// Add a tile layer from OpenStreetMap\nL.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {\n    attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors"\n}).addTo(map);\n\n// Add a marker\nvar marker = L.marker([51.5, -0.09]).addTo(map)\n    .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")\n    .openPopup();
+// Initialize the map
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+// Add a tile layer from OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Add a marker
+var marker = L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
